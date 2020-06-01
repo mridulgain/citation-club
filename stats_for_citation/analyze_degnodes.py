a070f4dcc23922f41b3c88d16840dfb3ec4eb36c
nandini <nandini.soni8@gmail.com>
2020-06-01 13:52:11 +0530

    documented python fie

--- stats_for_citation/analyze_degnodes.py
+++ stats_for_citation/analyze_degnodes.py
@@ -1,16 +1,37 @@
-# get list of coauthors
-# get total citations [out degree]
-# get total references [in degree]
-# get clubs
-# get club citations
+# how to run the file
+# uncomment the dataframe lines or list of ids and call the respective calculate function
+# in the console, python3 analyze_degnodes.py > path_to_json_file 
+
+# change line 84
+# change line 14, 15, 
 
 import pandas as pd
 import json
-from in_data import in_data_file
-
-data = json.load(open('../../analysis/data/data.json'))
 
 
+
+# calculates if paper is in journal or not
+path = '../../analysis/'
+data = json.load(open(path+'data/data.json'))
+
+def in_data_file(list_, new_li):
+    # print(list_)
+    li = list(list_.keys())
+    list_of_papers = {}
+    data = json.load(open(path+'data/data.json'))
+    for id in li:
+        for element in data:
+            if id == element['id']:
+                # print(id, list_[id])
+                try:
+                    list_of_papers[id] = new_li['x'][list_[id]]
+                except:
+                    list_of_papers[id] = new_li['vector'][list_[id]]
+                break
+    return list_of_papers
+
+
+# gets list of paper ids written by author id
 def papers(auth_id):
     papers_list = []
     for element in data:
@@ -20,6 +41,7 @@ def papers(auth_id):
     return papers_list
 
    
+# gets coauthors (dictonary paper_id: [coauth details]) of the given paper
 def coauth_paper(paper_id):
     coauth = {}
     for element in data:
@@ -31,8 +53,8 @@ def coauth_paper(paper_id):
         return None
 
 
+# gets all coautors of the author. [degree value in colaaboration network]
 def coauthors(auth_id):
-
     coauth = {}
     for element in data:
             for ele in element['authors']:
@@ -43,6 +65,7 @@ def coauthors(auth_id):
 
 
 
+# gets the paper ids and author ids of the paper's citation [out degree] of a given author. 
 def citations(auth_id, papers_list):
     cits = {}
     cit_auth = []
@@ -58,6 +81,7 @@ def citations(auth_id, papers_list):
     return cits, cit_auth
 
 
+# gets all the clubs a author is in
 def get_clubs(auth_id):
     clubs = []
     with open('../../experiment1/jmlr.clubs.txt') as club_file:
@@ -70,6 +94,7 @@ def get_clubs(auth_id):
                 clubs.append(temp3) 
     return clubs
 
+# gets a list of all papers and their authors who have cited a particular paper [in degree]
 def get_references(papers_):
     references = {}
     ref_auth = []
@@ -83,16 +108,19 @@ def get_references(papers_):
                 pass
     return references, ref_auth
 
+
+# gets author ids from all the citation coauthors of a given author
 def get_ids_from_citauth(auth_id, all_data):
     list_cit_coauth = []
     for element in all_data[auth_id]['citation_coauth']:
         if element is not None:
             for ele in element.values():
                 for e in ele:
-                    # print(e['id'])
                     list_cit_coauth.append(e['id'])
     return(list_cit_coauth)
     
+
+# gets total number of citations obtained fron the club
 def number_club_citations(cit_auth, club_auth, all_data):
     # print(cit_auth)
     sum_ = 0
@@ -104,10 +132,14 @@ def number_club_citations(cit_auth, club_auth, all_data):
     # print(sum_)
     return sum_
 
+
+# ets total number of citation from the journal papers
 def number_total_citations(auth_id, all_data):
     sum_ = len(all_data[auth_id]['ref_auth'])
     return sum_
 
+
+# main function which calculates the parameters for a given set of author ids [dataframe] and returns a json variable
 def calculate_all(value):
     all_data = {}
     for i in range(5):
@@ -127,11 +159,7 @@ def calculate_all(value):
     data_json = json.dumps(all_data)
     print(data_json)
 
-
-
-
-
-'''
+# same as calculate_all but here the input in a list of author ids
 def calculate(values):
     all_data = {}
     for i in values:
@@ -150,37 +178,8 @@ def calculate(values):
     data_json = json.dumps(all_data)
     print(data_json)
 
-# values = [179778838,92438100,1963576484,2148425697,	2467740887,	2150527135,] # degree 20
-# values = [134769616,563069026,2619985137,2100767082,2038901907] # degree 10
-# values =[2609987651, 2122328552, 1972291593, 1572478601, 2038901907, 60139091, 297432538, 168172700, 1980701853]
-# calculate(values)
-'''
 
-
-### uncomment and run.
-# deg_coauth = pd.read_csv('../../analysis/degreenodes_graph/degree_coauth.csv')
-# ei_coauth =  pd.read_csv('../../analysis/degreenodes_graph/eigen_coauth.csv')
-# btw_coauth =  pd.read_csv('../../analysis/degreenodes_graph/between_coauth.csv')
-# sort_deg_coauth = deg_coauth.sort_values(by=['x'], ascending = False).head(5)
-# sort_ei_coauth = ei_coauth.sort_values(by=['vector'], ascending = False).head(5)
-# sort_btw_coauth = btw_coauth.sort_values(by=['x'], ascending = False).head(5)
-# calculate_all(sort_deg_coauth)
-# calculate_all(sort_ei_coauth)
-# calculate_all(sort_btw_coauth)
-
-
-
-
-
-
-'''For papers will improve later'''
-'''for papers
-get coauthors
-get clubs
-get citations (out degree)
-get references (in degree)
-'''
-
+# checks whether a paper is in the journal or not
 def pre_processing_for_cit(df):
     li = df.head(80).to_dict()
     new_lis = {v:k for k,v in li['Unnamed: 0'].items()}
@@ -188,6 +187,7 @@ def pre_processing_for_cit(df):
     new_li = {x:y for x,y in zip(list(li.keys())[:10], list(li.values())[:10])}
     return new_li
 
+# returns just a list of coauthor ids of a given author id. to be run after calling coauth_paper()
 def after_coauth(auth):
     # print(auth)
     au = []
@@ -196,7 +196,9 @@ def after_coauth(auth):
             au.append(e['id'])
     return au
 
+# gets club citations for a paper
 def get_club_cit_paper(club_auth, cit_auth):
+    # print(len(club_auth))
     # print(club_auth)
     # print(cit_auth)
     sum_ = 0
@@ -204,14 +206,17 @@ def get_club_cit_paper(club_auth, cit_auth):
         if element is not None:
             # print(list(element.values())[0])
             for ele in list(element.values())[0]:
-                if ele['id'] in club_auth:
-                    # print(ele['id'])
-                    sum_ += 1
+                for e in club_auth:
+                    if ele['id'] in e:
+                        # print(ele['id'])
+                        sum_ += 1
     return sum_
 
+# gets journal citation for a paper
 def get_jour_cit(auth, all_data_papers, pap_id):
     return len(list(all_data_papers[str(pap_id)]['ref_auth'].values())[0])
 
+# calculates the parameters for different paper values [dataframe] returns a json
 def calculate_all_papers(paper_values):
     # all_data[str(value.iloc[i][0])] = {'coauthors': c, 'citations': a, 'citation_coauth': b, 'clubs': cl, 'references': r, 'ref_auth': ra}
     # print(paper_values)
@@ -222,7 +227,7 @@ def calculate_all_papers(paper_values):
         citation = {}
         cit_auth = {}
         r , ra = {}, {}
-        all_data_papers[str(paper_id)] = {'degree': paper_values[paper_id] , 'coauthors': coauth}
+        all_data_papers[str(paper_id)] = {'centrality value': paper_values[paper_id] , 'coauthors': coauth}
         au = after_coauth(coauth)
         pap_au = {}
         for a in au:
@@ -235,13 +240,9 @@ def calculate_all_papers(paper_values):
         club_cit = {}
         jour_cit = {}
         for a in au:
-            club_cit[a] = get_club_cit_paper(club[a][0], cit_auth[a])
+            club_cit[a] = get_club_cit_paper(club[a], cit_auth[a])
             jour_cit[a] = get_jour_cit(a, all_data_papers, paper_id)
         all_data_papers[str(paper_id)].update({'club_citation': club_cit, 'journal_citations': jour_cit})
-
-
-        
-
 
 
         
@@ -250,16 +251,53 @@ def calculate_all_papers(paper_values):
     return(data_json)
 
 
-ei_cit =  pd.read_csv('../../analysis/degreenodes_graph/eigen_cit.csv').sort_values(by=['vector'], ascending = False)
-btw_cit =  pd.read_csv('../../analysis/degreenodes_graph/between_cit.csv').sort_values(by=['x'], ascending = False)
-deg_cit = pd.read_csv('../../analysis/degreenodes_graph/degree_cit.csv').sort_values(by=['x'], ascending = False)
+'''values to be calculated for'''
+'''if author ids in a list, use calculate()'''
+''' change  file paths'''
 
 
+# values = [179778838,92438100,1963576484,2148425697,	2467740887,	2150527135,] # author ids with degree 20
+# values = [134769616,563069026,2619985137,2100767082,2038901907] # degree 10
+# calculate(values)
+
+
+### uncomment and run direct each output to a json file, so uncomment and run 2 lines at a time
+
+# deg_coauth = pd.read_csv(path+'degreenodes_graph/degree_coauth.csv')
+# ei_coauth =  pd.read_csv(path+'degreenodes_graph/eigen_coauth.csv')
+# btw_coauth =  pd.read_csv(path+'degreenodes_graph/between_coauth.csv')
+
+# unccomment the above lines
+
+# sort_deg_coauth = deg_coauth.sort_values(by=['x'], ascending = False).head(5)
+# calculate_all(sort_deg_coauth)
+
+# sort_ei_coauth = ei_coauth.sort_values(by=['vector'], ascending = False).head(5)
+# calculate_all(sort_ei_coauth)
+
+# sort_btw_coauth = btw_coauth.sort_values(by=['x'], ascending = False).head(5)
+# calculate_all(sort_btw_coauth)
+
+
+
+
+
+
+### uncomment and run direct each output to a json file, so uncomment and run 2 lines at a time
+
+# ei_cit =  pd.read_csv(path+'degreenodes_graph/eigen_cit.csv').sort_values(by=['vector'], ascending = False)
+# btw_cit =  pd.read_csv(path+'degreenodes_graph/between_cit.csv').sort_values(by=['x'], ascending = False)
+deg_cit = pd.read_csv(path+'degreenodes_graph/degree_cit.csv').sort_values(by=['x'], ascending = False)
+
+# unccomment the above lines
 final_deg_dict = pre_processing_for_cit(deg_cit)
-final_ei_dict = pre_processing_for_cit(ei_cit)
-final_btw_dict = pre_processing_for_cit(btw_cit)
-
-# print(final_deg_dict)
 value = calculate_all_papers(final_deg_dict)
 print(value)
-# ['1647376582']
+
+# final_ei_dict = pre_processing_for_cit(ei_cit)
+# value = calculate_all_papers(final_ei_dict)
+# print(value)
+
+# final_btw_dict = pre_processing_for_cit(btw_cit)
+# value = calculate_all_papers(final_btw_dict)
+# print(value)
