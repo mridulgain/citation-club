0146087a2296fc55d81a56f461a7791408edf15d
rohan <rohan@email.com>
2020-06-05 16:53:26 +0530

    fixed experiment3

--- experiment3.py
+++ experiment3.py
@@ -6,6 +6,7 @@
 import pickle
 import clubDetector as cd
 
+journal = "jmlr"
 
 def getCommunities(fnam, directed):
     g = cd.input_edgelist(fnam, directed)
@@ -16,18 +17,41 @@ def getCommunities(fnam, directed):
     communities = cd.detect_communities(gcc, directed)
     ###
     if directed:
-        pickle.dump(communities, open("./results/experiment3/prl.communities_cit.bin", 'wb'))
+        pickle.dump(communities, open("./results/experiment3/"+journal+".communities_cit.bin", 'wb'))
     else:
-        pickle.dump(communities, open("./results/experiment3/prl.communities_co.bin", 'wb'))
+        pickle.dump(communities, open("./results/experiment3/"+journal+".communities_co.bin", 'wb'))
     ###
-    return cd.prepare_output("./data/prl/prl_dblp.json", communities, directed)
+    return cd.prepare_output("./data/"+journal+"/"+journal+"_dblp.json", communities, directed)
+
+
+def getCommunitiesFromFiles(fnam, directed):
+    communities = pickle.load(open(fnam, 'rb'))
+    return cd.prepare_output("./data/"+journal+"/"+journal+"_dblp.json", communities, directed)
+
+
+def filter(comm)->list:
+        i = 0
+        while i < len(comm):
+            if len(comm[i]) < 4:
+                comm.pop(i)
+            else:
+                i += 1
+        return comm
 
 if __name__ == '__main__':
-    # getting communities
-    c1 = getCommunities("./data/prl/el_cit", True)
-    c2 = getCommunities("./data/prl/el_co", False)
+    '''# getting communities
+    c1 = getCommunities("./data/"+journal+"/el_cit", True)
+    c2 = getCommunities("./data/"+journal+"/el_co", False)
     # getting clubs
-    cd.write_communities(c1, "./results/experiment3/prl.communities_cit.txt")
-    cd.write_communities(c2, "./results/experiment3/prl.communities_co.txt")
+    cd.write_communities(c1, "./results/experiment3/"+journal+".communities_cit.txt")
+    cd.write_communities(c2, "./results/experiment3/"+journal+".communities_co.txt")'''
+    c1 = getCommunitiesFromFiles("./results/experiment3/"+journal+".communities_cit.bin", True)
+    c2 = getCommunitiesFromFiles("./results/experiment3/"+journal+".communities_co.bin", False)
+    print("before pruning", len(c1), len(c2))
+    c1 = filter(c1)
+    c2 = filter(c2)
+    print("after pruning", len(c1), len(c2))
     c = cd.community_intersection(c1, c2)
-    cd.write_clubs(c, './results/experiment3/prl.clubs.txt')
+    #print(c)
+    print(len(c))
+    cd.write_clubs(c, './results/experiment3/'+journal+'.clubs.txt')
