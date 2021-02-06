3ab8eb7e30b361e6473df06ba1d88e5dbc0bbdcb
Mridul Gain <mridulgain@gmail.com>
2021-02-07 00:38:23 +0530

    pcc size vs avg strength

---
+++ analysis3.py
@@ -0,0 +1,23 @@
+# size vs strength
+import sys
+import pickle
+from collections import Counter
+
+try:
+    journal = sys.argv[1]
+except IndexError:
+    #journal = "prl"
+    print("usage: python3 club_freq_distrib.py [jmlr|prl]")
+    exit(0)
+
+pccs = pickle.load(open("./results/experiment1/" + journal + ".pcc_details.bin", 'rb'))
+freq = Counter(len(x[0]) for x in pccs)
+strength = {}
+for x in pccs:
+    strength[len(x[0])] = strength.setdefault(len(x[0]), 0) + x[2]
+# avg
+for x in strength:
+    strength[x] /= freq[x]
+# output
+for i in strength:
+    print(i, strength[i])
