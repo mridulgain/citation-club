dd231fc8c54eb158be940e55461c64b79ca179c8
Mridul Gain <mridulgain@gmail.com>
2021-02-07 00:38:02 +0530

    club size histogram

--- analysis2.py
+++ analysis2.py
@@ -1,31 +1,17 @@
-# calculate and store pcc details for all pccs for a given journal
-from clubAnalyser import Analysis
+# member distribution in pccs
 import sys
 import pickle
+from collections import Counter
 
 try:
     journal = sys.argv[1]
 except IndexError:
     #journal = "prl"
-    print("usage: python3 experiment3.py [jmlr|prl]")
+    print("usage: python3 club_freq_distrib.py [jmlr|prl]")
     exit(0)
 
-db = "./data/" + journal + "/" + journal + "_dblp.json"
+pccs = pickle.load(open("./results/experiment1/" + journal + ".pcc_details.bin", 'rb'))
 
-pccs = pickle.load(open('./results/experiment1/' + journal + '.clubs.bin', 'rb'))
-#score = []
-pcc_scc_score = []
-for c in pccs:
-    ca = Analysis(list(c), db)
-    sccs = ca.get_scc()
-    total_strength = 0
-    scc_list = []
-    for i in sccs:
-        total_strength += i.weight
-        
-    print(len(c), total_strength)
-    score.append(total_strength)
-    pcc_scc_score.append([c, sccs, total_strength])
-#print("max strength", max(score))
-#pickle.dump(score, open("./results/experiment1/" + journal + ".score.bin", 'wb'))
-pickle.dump(pcc_scc_score, open("./results/experiment1/" + journal + ".pcc_scc_score.bin", 'wb'))
+freq = Counter(len(x[0]) for x in pccs)
+for i in freq:
+    print(i, freq[i])
